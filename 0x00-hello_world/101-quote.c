a92cf04eff0eaa81d7139fafbf2704f46e5777e4
Daniel Azil <danielazil.edu@gmail.com>
2023-03-16 22:18:12 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-# include <stdio>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Discriptot = std
- * bytes allocated to strings = 40 bytes
- * Return: Always 0 (Success)
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(std, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"40);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 	return (1);
 }
