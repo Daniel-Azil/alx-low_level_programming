99ed8760380e7f9059279faf52fdae8755ae36ea
Daniel Azil <danielazil.edu@gmail.com>
2023-03-21 19:35:50 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  function that prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ * Return: nothing.
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours < 24; ++hours)
+	{
+		for (minutes = 0; minutes < 60; ++minutes)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
