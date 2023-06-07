518cb8ad77aa962093faddc9fbcbbf8c1fed8d04
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-06-07 21:54:50 +0000

    made some corrections

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,12 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0') {
+	if (*s == '\0')
 		return (0);
-	}
-	else {
+	else
 		return (1 + _strlen_recursion(s + 1));
-	}
 }
 
 /**
@@ -25,11 +23,10 @@ int _strlen_recursion(char *s)
 
 int comparator(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *(s + n2)) {
+	if (*(s + n1) == *(s + n2))
 		if (n1 == n2 || n1 == n2 + 1)
 		return (1);
 		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
 	return (0);
 }
 
@@ -40,8 +37,7 @@ int comparator(char *s, int n1, int n2)
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0') {
+	if (*s == '\0')
 	return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
-	}
 }
