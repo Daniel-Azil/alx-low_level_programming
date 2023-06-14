4e69f9ec4e6b615c714ee3618f6a882738d879a9
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2023-06-14 04:49:22 +0000

    corrections to betty linter errors

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
  * free_grid - deallocates memory for a 2D array grid.
  * @grid: 2D array of integers.
  * @height: height of the grid.
- * Returns: no return value.
+ * Return: no return value.
  */
 
 void free_grid(int **grid, int height)
