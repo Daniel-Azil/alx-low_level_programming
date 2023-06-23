da3e06bf2db179131d0bb94f569c57612d5dca46
Daniel Azil <danielazil.fse@gmail.com>
2023-06-23 01:12:02 +0000

    Fixed issues in print_all function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,36 +7,36 @@
  * Return: void
  */
 void print_all(const char * const format, ...) {
-    int i = 0;
-    char *sep = "";
-    va_list args;
-    va_start(args, format);
-    while (format[i]) {
-        if (format[i] == 'c') {
-            printf("%s%c", sep, va_arg(args, int));
-            sep = ", ";
+	int i = 0;
+	char *sep = "";
+	va_list args;
+	va_start(args, format);
+	while (format[i]) {
+		if (format[i] == 'c') {
+			printf("%s%c", sep, va_arg(args, int));
+			sep = ", ";
 
-        } else if (format[i] == 'i') {
-            printf("%s%i", sep, va_arg(args, int));
-            sep = ", ";
+		} else if (format[i] == 'i') {
+			printf("%s%i", sep, va_arg(args, int));
+			sep = ", ";
 
-        } else if (format[i] == 'f') {
-            printf("%s%f", sep, va_arg(args, double));
-            sep = ", ";
+		} else if (format[i] == 'f') {
+			printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
 
-        } else if (format[i] == 's') {
-            char *s = va_arg(args, char *);
+		} else if (format[i] == 's') {
+			char *s = va_arg(args, char *);
 
-            if (s == NULL) {
-                printf("%s(nil)", sep);
+			if (s == NULL) {
+				printf("%s(nil)", sep);
 
-            } else {
-                printf("%s%s", sep, s);
-            }
-            sep = ", ";
-        }
-        i++;
-    }
-    va_end(args);
-    puts("");
+			} else {
+				printf("%s%s", sep, s);
+			}
+			sep = ", ";
+		}
+		i++;
+	}
+	va_end(args);
+	puts("");
 }
